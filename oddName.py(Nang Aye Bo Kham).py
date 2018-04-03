e476248780bf78b990bf8cd7f5250fecf127ffe6
ayebo <31093794+ayebo@users.noreply.github.com>
2018-04-03 14:25:26 +0800

    "Add name printing program"

---
+++ oddName.py(Nang Aye Bo Kham).py
@@ -0,0 +1,12 @@
+def main():
+
+    name=(input("Enter your name here"))
+    #This is a comment
+
+    while len(name)<=1:
+        print("Error")
+        name=(input("Enter your name here"))
+
+        print(name[::2])
+
+        main()
